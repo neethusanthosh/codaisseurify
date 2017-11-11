b92dd9ef9bbbf5b37b317e3666d5fc8253a629a3
NEETHU <neethusanthosh01@gmail.com>
2017-11-12 00:20:09 +0100

    crud operations on artist

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  #root    'artists#index'
+  resources :artists
 
-  get "artists" => 'artists#index'
-  get "artists/:id" => "artists#show", as: :artist
-  # get 'artists', to:'pages#artists'
+  # get "artists" => 'artists#index'
+  # get 'artists/new' => "artists#new", as: :new_artist
+  # get "artists/:id" => "artists#show", as: :artist
+  # post "artists" => "artists#create"
+  # get 'artists/:id/edit' => 'artists#edit', as: :edit_artist
+  # patch "artists/:id" => "artists#update"
+  # delete "artists/:id" => "artists#destroy"
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
