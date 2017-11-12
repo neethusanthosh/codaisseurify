b872a42bda2cdfaea6b8e5af70dd3320d8bdf3a0
NEETHU <neethusanthosh01@gmail.com>
2017-11-12 19:22:47 +0100

    Model associations tested

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+# RSpec.describe Artist, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+
+describe "association with song" do
+  let(:artist) { create :artist }
+  let!(:song) { create :song, artist: artist }
+
+  it "has many songs" do
+    song1 = artist.songs.new(title: "Dil se")
+    song2 = artist.songs.new(title: "Roja")
+
+    expect(artist.songs).to include(song1)
+    expect(artist.songs).to include(song2)
+  end
+
+  it "deletes associated songs" do
+    expect { artist.destroy }.to change(Song, :count).by(-1)
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Song, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+#RSpec.describe Song, type: :model do
+  #pending "add some examples to (or delete) #{__FILE__}"
+#end
+
+describe "association with artist" do
+  let(:artist) { create :artist }
+
+  it "belongs to a artist" do
+    song = artist.songs.build(title: "")
+
+    expect(song.artist).to eq(artist)
+  end
+
+  # OR, go the shoulda way:
+
+    
 end
